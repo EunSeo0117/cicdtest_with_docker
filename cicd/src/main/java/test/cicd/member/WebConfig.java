775c2f39865b2d8c01bd9f9f5e19797c771174ce
EunSeo0117 <gmjea023@gmail.com>
2025-03-15 19:33:12 +0900

    chore : dockerfile 생성

---
+++ cicd/src/main/java/test/cicd/member/WebConfig.java
@@ -0,0 +1,13 @@
+package test.cicd.member;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("*"); // 모든 도메인 허용
+    }
+}
